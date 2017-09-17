5d30fad7c820f02eb33baa598253a536a5d48d7b
yanaM0nkey <ya.zenkevich@gmail.com>
2017-09-17 18:02:13 +0300

    .

---
+++ CW22/src/cw22/CW22.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cw22;
+
+/**
+ *
+ * @author UX501
+ */
+public class CW22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int get;
+       int set;
+       int bytes;
+       int finallist;
+      
+       int $int;
+       Integer a = 130;
+       Integer b = 130;
+       
+        System.out.println(a==b);
+        
+        /*int a = 10;
+        switch(a){
+            case 5:
+                System.out.println("5");
+            case 10:
+                System.out.println("10");
+            case 15:
+                System.out.println("15");
+                default:
+                    System.out.println("default");
+        }*/
+        
+        
+        }
+    }
+    
+}
